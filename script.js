2670a03c122ea85483fb39624d58ee7f648b52ab
Greg.Wallerus <gwallerus@gmail.com>
2017-09-04 12:29:10 -0500

    first commit

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-//The application should have an input form that collects employee first name, last name, ID number,
-// job title, annual salary.
- //A 'Submit' button should collect the form information, store the information to
- //calculate monthly costs, append information to the DOM and clear the input fields.
- //Using the stored information, calculate monthly costs and append this to the to DOM.
+$(document).ready(onReady)
 
-//function Employee(firstName, lastName, idNumber, title, annSalary){
-   // this.firstName = firstName;
-    //this.lastName = lastName;
-    //this.idNumber = idNumber;
-    //this.title = title;
-    //this.annSalary = annSalary;
-
-
-//Create an application that records employee salaries and adds salaries up to report monthly costs.
-$(document).ready(function(){
-
-$('#input').on('click', employee());
-
-});
-
-function employee(){
-
-    
+function onReady() {
+    $('.dataCollect').on('click', dataCollect);
 }
+
+var employees = [];
+
+function worker (firstIn, lastIn, idIn, titleIn, salaryIn) {
+    this.firstname = firstIn;
+    this.lastname = lastIn;
+    this.idnumber = idIn;
+    this.jobtitle = titleIn;
+    this.annualsalary = salaryIn;
+    this.monthlyCost = (salaryIn / 12);
+    employees.push(this);
+
+function dataCollect(){
+
+}
+
